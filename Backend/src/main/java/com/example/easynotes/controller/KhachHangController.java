38d84412b164556b9a47cd435db68361ab2655f3
VanVinh983 <doanvinh983@gmail.com>
2022-04-19 06:23:36 +0700

    add api dangkytaikhoan in api khach hang

--- Backend/src/main/java/com/example/easynotes/controller/KhachHangController.java
+++ Backend/src/main/java/com/example/easynotes/controller/KhachHangController.java
@@ -3,13 +3,16 @@ package com.example.easynotes.controller;
 
 import com.example.easynotes.exception.ResourceNotFoundException;
 import com.example.easynotes.model.KhachHang;
+import com.example.easynotes.model.TaiKhoan;
 import com.example.easynotes.service.KhachHangService;
+import com.example.easynotes.service.TaiKhoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by rajeevkumarsingh on 27/06/17.
@@ -21,6 +24,9 @@ public class KhachHangController  {
     @Autowired
     private KhachHangService service;
 
+    @Autowired
+    private TaiKhoanService taiKhoanService;
+
     @GetMapping("/")
     public List<KhachHang> getAll() {
         return service.getList();
@@ -29,6 +35,18 @@ public class KhachHangController  {
     @PostMapping("/")
     public KhachHang addItem(@Valid @RequestBody KhachHang khachHang) {
         return service.add(khachHang);
+    }
+        @PostMapping("/dangkytaikhoan/")
+    public KhachHang addAccount(@Valid @RequestBody KhachHang khachHang) {
+        List<KhachHang> kh0 = service.getList();
+        taiKhoanService.add(khachHang.getTaiKhoan());
+        service.add(khachHang);
+        List<KhachHang> kh1 = service.getList();
+        if (kh0.stream().count() < kh1.stream().count()){
+            return khachHang;
+        }else {
+            return new KhachHang();
+        }
     }
 
     @GetMapping("/{id}")
